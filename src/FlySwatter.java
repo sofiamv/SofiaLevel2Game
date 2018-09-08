a11eec247a9dffbf773e1258212241eb78ef33de
isabelluu <league@iMac-2.attlocal.net>
2018-09-08 15:57:02 -0700

    STEP 6 PART 7
    AOTGHWEROTHWURAH CNRUIA NCKJHFSDAN CRIUA NTVISRAGCM≈©ÅÁGEWV
    NRGRYDSMFVGAUIXMDRUEWARFGNV RUSMXDYAIWEHRYICFMRAWEUIAOYHRC89YWR,C4MU
    895UMA9RGU8RM Y79YR79TYV 39ETVSEVYG;U TSB’
    BROYS[EDOFUTMA=SERMBYU8T-EUSM5E08UY08TDH

--- src/FlySwatter.java
+++ src/FlySwatter.java
@@ -1,4 +1,29 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
 
 public class FlySwatter {
-
+	JFrame frame;
+	final int WIDTH = 1100;
+	final int HEIGHT = 700;
+	GamePanel gp;
+FlySwatter(){
+	frame = new JFrame();
+	gp = new GamePanel();
+}
+public static void main(String[] args) {
+	FlySwatter fs = new FlySwatter();
+	fs.setup();
+	
+}
+public void setup() {
+	frame.addKeyListener(gp);
+	frame.add(gp);
+	frame.setVisible(true);
+	frame.setSize(WIDTH, HEIGHT);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.getContentPane().setPreferredSize(new Dimension(WIDTH, HEIGHT));
+	frame.pack();
+	gp.StartGame();
+}
 }
--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,4 +1,50 @@
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class GamePanel {
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class GamePanel extends JPanel implements ActionListener, KeyListener{
+Timer t;
+GameObject go;
+GamePanel(){
+ t = new Timer(1000/90, this);
+ go = new GameObject(0, 0, 20, 20);
+}
+public void StartGame() {
+	t.start();
+}
+@Override
+
+public void paintComponent(Graphics g){
+               go.draw(g);
+
+        }
+
+
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+	go.update();
+	repaint();
+}
+@Override
+public void keyTyped(KeyEvent e) {
+	// TODO Auto-generated method stub
+	System.out.println("1 :(");
+}
+@Override
+public void keyPressed(KeyEvent e) {
+	// TODO Auto-generated method stub
+	System.out.println("2 D:");
+}
+@Override
+public void keyReleased(KeyEvent e) {
+	// TODO Auto-generated method stub
+	System.out.println("3 D':");
+}
 
 }
